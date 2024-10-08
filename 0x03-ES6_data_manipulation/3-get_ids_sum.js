05019d55d55276b5e037200111105c0642bbbf6e
mhlibya <mhlibya728@gmail.com>
2024-10-08 22:48:44 +0200

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,9 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
-
-export default function getStudentIdsSum(array) {
-    let ids = getListStudentIds(array)
-    let sum = 0;
-    for(let i = 0; i < ids.length; i++) {
-        sum += array[i];
-    }
-    return sum;
-}
+/* eslint-disable no-unused-vars */
+export default function getStudentIdsSum(getListStudents) {
+    const myArr = [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return myArr.reduce(((reducer, myArr) => reducer + myArr.id), 0);
+  }
