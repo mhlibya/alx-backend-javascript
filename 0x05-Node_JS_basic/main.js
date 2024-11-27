84ba6cc47c99cdb8bf93b565c4760511314a2ed8
mhlibya <mhlibya728@gmail.com>
2024-11-27 03:21:53 +0200

    3-read_file_async.js

--- 0x05-Node_JS_basic/main.js
+++ 0x05-Node_JS_basic/main.js
@@ -1,3 +1,9 @@
-const displayMessage = require('./0-console');
+const countStudents = require('./3-read_file_async');
 
-displayMessage("Hello NodeJS!");
+countStudents("nope.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
+    });
