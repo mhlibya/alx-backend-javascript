814e160e21408e97995c29bce0fe46deab94b2f9
mhlibya <mhlibya728@gmail.com>
2024-11-27 03:08:12 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 //asking the user and answering
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim(); // Trim whitespace and newline characters
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-process.on("exit", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
